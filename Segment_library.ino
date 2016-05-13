9c59a31570a18d430c43d694550345ca60d60187
OnkelDittmeyer <helloWorld@marcelhelmer.de>
2016-05-13 17:22:54 +0100

    bugfix

---
+++ Segment_library.ino
@@ -0,0 +1,160 @@
+void writeDisplay(int number){
+  long emptyBinary = 0b11111111; //binary for an empty digit display
+  String numberString = String(number);
+  
+  // walk through digits backwards and write them into displays
+  if(numberString.length() <= numberOfDigits){
+    for(int i = numberString.length()-1; i >= 0 ; i--){
+      writeReg(convertNumber(number, i));
+    }
+//      //fill left displays with empty space
+//      if(numberString.length() < numberOfDigits){
+//        for(int n= 0; n < (numberOfDigits - numberString.length()); n++){
+//
+//            writeReg(emptyBinary);
+//          
+//          }  
+//      }
+    
+  }else{
+    Serial.println("Number too large for display segments");  
+  }
+
+
+  
+}
+
+
+long convertNumber(int number, int pos){
+  String numberString = String(number);
+  pos = pos-1;
+  if(pos < numberString.length()){
+    switch(numberString.charAt(pos)){
+      case '0':
+        return 0b11000000;  //change to bitcode for 0
+        break;
+      case '1':
+        return 0b11101101;  // change to bitcode for 1
+        break;
+      case '2':
+        return 0b10110000;  // change to bitcode for 2
+        break;
+      case '3':
+        return 0b10101000;  // change to bitcode for 3
+        break;
+      case '4':
+        return 0b10001101;  // change to bitcode for 4
+        break;
+      case '5':
+        return 0b10001010;  // change to bitcode for 5
+        break;
+      case '6':
+        return 0b10000010;  // change to bitcode for 6
+        break;                                     
+      case '7':
+        return 0b11101100;  // change to bitcode for 7
+        break;
+      case '8':
+        return 0b00000000;  // change to bitcode for 8
+        break; 
+      case '9':
+        return 0b00001000;  // change to bitcode for 9
+        break;
+      default:
+        return 0b11111111;  // change to bitcode for nothing
+        break;
+                           
+      }
+    
+    
+    }else{
+      return 0;  
+    }
+}
+
+
+void clearDisplays(){
+    long binaryClear = 0b11111111; //change to binary for all out
+
+    for(int i=0; i<numberOfDigits; i++){
+        writeReg(binaryClear);
+    }
+  }
+
+void writeReg(long regNum){
+    digitalWrite(latchPin, 0);
+    //move 'em out
+    shiftOut(dataPin, clockPin, regNum);   
+    //shiftOut(dataPin, clockPin, regNum2);
+    //shiftOut(dataPin, clockPin, regNum3);
+    //return the latch pin high to signal chip that it 
+    //no longer needs to listen for information
+    digitalWrite(latchPin, 1);
+    //delay(300); // uncomment this delay ---------------------------------------------------------------------------
+
+}
+
+void writeRegAll(long regNum, long regNum2, long regNum3){
+    digitalWrite(latchPin, 0);
+    //move 'em out
+    shiftOut(dataPin, clockPin, regNum);   
+    shiftOut(dataPin, clockPin, regNum2);
+    shiftOut(dataPin, clockPin, regNum3);
+    //return the latch pin high to signal chip that it 
+    //no longer needs to listen for information
+    digitalWrite(latchPin, 1);
+    //delay(300); // uncomment this delay ---------------------------------------------------------------------------
+
+}
+
+
+void shiftOut(int myDataPin, int myClockPin, byte myDataOut) {
+  // This shifts 8 bits out MSB first, 
+  //on the rising edge of the clock,
+  //clock idles low
+
+  //internal function setup
+  int i=0;
+  int pinState;
+  pinMode(myClockPin, OUTPUT);
+  pinMode(myDataPin, OUTPUT);
+
+  //clear everything out just in case to
+  //prepare shift register for bit shifting
+  digitalWrite(myDataPin, 0);
+  digitalWrite(myClockPin, 0);
+
+  //for each bit in the byte myDataOut�
+  //NOTICE THAT WE ARE COUNTING DOWN in our for loop
+  //This means that %00000001 or "1" will go through such
+  //that it will be pin Q0 that lights. 
+  for (i=7; i>=0; i--)  {
+    digitalWrite(myClockPin, 0);
+
+    //if the value passed to myDataOut and a bitmask result 
+    // true then... so if we are at i=6 and our value is
+    // %11010100 it would the code compares it to %01000000 
+    // and proceeds to set pinState to 1.
+    if ( myDataOut & (1<<i) ) {
+      pinState= 1;
+    }
+    else {  
+      pinState= 0;
+    }
+
+    //Sets the pin to HIGH or LOW depending on pinState
+    digitalWrite(myDataPin, pinState);
+    //register shifts bits on upstroke of clock pin  
+    digitalWrite(myClockPin, 1);
+    //zero the data pin after shift to prevent bleed through
+    digitalWrite(myDataPin, 0);
+  }
+
+  //stop shifting
+  digitalWrite(myClockPin, 0);
+}
+
+
+
+
+
---
+++ sn75441_3xhc595_stepper_segment.ino
@@ -0,0 +1,64 @@
+
+
+#include <Stepper.h>
+
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+// for your motor
+
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+int stepCount = 0;  // number of steps the motor has taken
+
+//shiftReg setup for segment display
+//Pin connected to ST_CP of 74HC595
+int latchPin = 5;
+//Pin connected to SH_CP of 74HC595
+int clockPin = 7;
+////Pin connected to DS of 74HC595
+int dataPin = 6;
+
+int displayNumber = 123; //number you want on the display
+int lastDisplayNumber = 111; //last displayNumber ---------------------------------------
+
+const int numberOfDigits = 3; //number of displays you use
+
+void setup() {
+  
+    pinMode(latchPin, OUTPUT);
+    pinMode(clockPin, OUTPUT);
+    pinMode(dataPin, OUTPUT);
+
+    clearDisplays();
+    Serial.begin(9600);
+    
+}
+
+void loop() {
+  // read the sensor value:
+  int sensorReading = analogRead(A0);
+  // map it to a range from 0 to 100:
+  int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
+  // set the motor speed:
+  if (motorSpeed > 0) {
+    myStepper.setSpeed(motorSpeed);
+    // step 1/100 of a revolution:
+    myStepper.step(stepsPerRevolution / 100); 
+  }
+
+  //update the display if the number changed, if not, dont update ----------------------
+  if(lastDisplayNumber != displayNumber){
+    // try all these functions on their own and check if anything changed
+    
+    //writeDisplay(4); //writes the standard functions
+
+    //writeRegAll(11101101, 10110000, 10101000); //writes all three numbers at the same time as binary, starts with the first digit 
+
+    //writeRegAll(convertNumber(123,3), convertNumber(123,2), convertNumber(123,1)); //convertNumber(number, position of number), this creates the binary dynamically
+    
+    lastDisplayNumber = displayNumber; 
+    }
+
+  
+}
